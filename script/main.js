37888d6801dcd143d02bdeb173fe7caafa72fbb6
Ines Haouala <ines.ha13@hotmail.fr>
2021-06-11 00:16:16 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -194,8 +194,8 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .from(".hat", 3, {
-      x: -100,
+    .from(".hat", 0.5, {
+      x: -150,
       y: 350,
       rotation: -160,
       opacity: 0
